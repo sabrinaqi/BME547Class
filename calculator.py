f784e1eb1d9683097ebe981e1430500fbe2137ee
sabrinaqi <sabrina.qi@duke.edu>
2020-01-17 14:45:16 -0500

    Added functions and print statements

--- calculator.py
+++ calculator.py
@@ -1,36 +1,33 @@
 # calculator.py
 
 def add(x,y):
-    z = x+y # this is a local variable, so z is not defined anywhere else in the code
-    print("{} + {} = {}".format(x,y,z))    
+    z = x+y # this is a local variable, so z is not defined anywhere else in the code  
     return z
 def subtract(x,y):
     z = x-y
-    print("{}-{}={}".format(x,y,z))
     return z
 def multiply(x,y):
     z = x*y
-    print("{}*{}={}".format(x,y,z))
+    return z
+def divide(x,y):
+    z = x/y
     return z
 
 x = input("Enter a letter: ")
 print("You entered {}".format(x))
-print("This is a line of code.")
 
 if x == "a":
-    d = add(100,250)
-    if d>100:
-        print("This number is too high.")
+    z = add(47,7)
+    print("{}+{} = {}".format(47,7,z))
 elif x =="b":
-    x = 20
-    y = -3
-    z = subtract(x,y)
-    print("{}-{} = {}".format(x,y,z))
+    z = subtract(47,7)
+    print("{}-{} = {}".format(47,7,z))
 elif x == "c":
-    x = 34
-    y = 32
-    z = multiply(x,y)
-    print("{}*{} = {}".format(x,y,z))
+    z = multiply(47,7)
+    print("{}*{} = {}".format(47,7,z))
+elif x == "d":
+    z = divide(47,7)
+    print("{}/{} = {}".format(47,7,z))
 else:
     print("The {} command is not recognized.".format(x))
 
